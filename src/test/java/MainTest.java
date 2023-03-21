d34c9c79c1b1cd780a16ec87cc6fbb4c4934f219
Rakshit Gupta <rakshitgupta602@gmail.com>
2023-03-21 23:29:11 +0530

    error fixedd

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -13,7 +13,7 @@ public class MainTest {
         assertEquals("power for Correct Output", 5, obj.power(5, 1),0.001);
         assertEquals("power for Correct Output", 16, obj.power(4, 2), 0.001);
         assertEquals("natural log for Correct Output", 1.6094379124341003, obj.log(5), 0.001);
-        assertEquals("natural log for 1Correct Output", 1.3862943611198906, obj.log(4),0.001);
+        assertEquals("natural log for Correct Output", 1.3862943611198906, obj.log(4),0.001);
         assertEquals("square root for Correct Output", 5, obj.SquareRoot(25),0.001);
         assertEquals("square root for Correct Output", 4, obj.SquareRoot(16),0.001);
         assertNotEquals("Square Root Test 1", 1, obj.SquareRoot(4), 0.001);
