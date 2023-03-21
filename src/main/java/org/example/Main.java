db5597c7ca47bb985d8ad574ee123da01705ab69
Rakshit Gupta <rakshitgupta602@gmail.com>
2023-03-21 20:18:50 +0530

    Added functions and test cases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,15 +8,62 @@ public class Main {
         Main obj= new Main();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("enter number please: ");
-        int n = scanner.nextInt();
-        System.out.println(obj.factorial(n));
+
+        while(true){
+            System.out.println("Enter 1 for factorial function, 2 for Square root function, 3 for logarithm function or 4 for power function:");
+            int inp = scanner.nextInt();
+
+            switch(inp) {
+                case 1:
+                    System.out.print("(Factorial Function) Enter a number : ");
+                    n1 = scanner.nextDouble();
+                    System.out.println("Factorial of "+n1+" is : " + obj.factorial(n1));
+                    System.out.println("\n");
+                    break;
+
+                case 2:
+                    System.out.print("(FSquareRoot Function) Enter a number : ");
+                    n1 = scanner.nextDouble();
+                    System.out.println("Square root of "+n1+" is : " + obj.SquareRoot(n1));
+                    System.out.println("\n");
+                    break;
+
+                case 3:
+                    System.out.print("(log Function) Enter a number : ");
+                    n1 = scanner.nextDouble();
+                    System.out.println("Natural Log of "+n1+" is : " + obj.log(n1));
+                    System.out.println("\n");
+                    break;
+
+                case 4:
+                    System.out.print("(Power Function) Enter a number1 : ");
+                    n1 = scanner.nextDouble();
+                    System.out.print("(Power Function) Enter a number2 : ");
+                    n2 = scanner.nextDouble();
+                    System.out.println(n1+ " raised to power "+n2+" is : " + obj.power(n1, n2));
+                    System.out.println("\n");
+                    break;
+            }
+        }
     }
-    public int factorial(int n){
-        int res =1;
-        for(int i=1;i<=n;i++){
+    public double factorial(double n){
+        double res =1;
+        for(double i=1;i<=n;i++){
             res = res*i;
         }
         return res;
+    }
+    public double SquareRoot(double n){
+        double res = Math.sqrt(n);
+        return res;
+
+    }
+
+    public double log(double n){
+        return Math.log(n);
+    }
+
+    public double power(double n1, double n2){
+        return Math.pow(n1,n2);
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -9,6 +9,12 @@ public class MainTest {
     public void factorialTruePositive(){
         assertEquals("calculating factorial",120,obj.factorial(5));
         assertEquals("calculating factorial",24,obj.factorial(4));
+        assertEquals("power for Correct Output", 5, obj.power(5, 1));
+        assertEquals("power for Correct Output", 16, obj.power(4, 2));
+        assertEquals("natural log for Correct Output", 1.6094379124341003, obj.naturalLog(5));
+        assertEquals("natural log for Correct Output", 1.3862943611198906, obj.naturalLog(4));
+        assertEquals("square root for Correct Output", 5, obj.squareRoot(25));
+        assertEquals("square root for Correct Output", 4, obj.squareRoot(16));
     }
 
 }
